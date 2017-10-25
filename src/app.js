24dc8a95ec7e1751a561e1e2184f26cfbe8b77b1
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-25 11:57:29 +0200

    Routing - First steps

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is my dashboard stateless component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is my add expense stateless component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/*exact true, if not it will match it everytime*/}
+            <Route path="/create" component={AddExpensePage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true // this is only for the development, if not client routing doesn't work
     }
 }
