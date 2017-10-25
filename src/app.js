f013f29e3dea9d35809bf74f253a58577c986fea
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-25 12:12:38 +0200

    Setting up 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -16,12 +16,33 @@ const AddExpensePage = () => (
     </div>
 );
 
+const EditExpensePage = () => (
+    <div>
+        This is my edit expense stateless component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my help expense stateless component
+    </div>
+);
+
+const NotFound404Page = () => (
+    <div>
+        404!
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/*exact true, if not it will match it everytime*/}
             <Route path="/create" component={AddExpensePage} />
-        </div>
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+            <Route component={NotFound404Page} />
+        </Switch>
     </BrowserRouter>
 );
 
