fc15b720af4fbcf1dfc1f291b3d6481a386c4cdc
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-25 15:27:39 +0200

    Query strings and url parameters

--- src/app.js
+++ src/app.js
@@ -2,64 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css';
+import AppRouter from './routers/AppRouter';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        This is my dashboard stateless component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is my add expense stateless component
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        This is my edit expense stateless component
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is my help expense stateless component
-    </div>
-);
-
-const NotFound404Page = () => (
-    <div>
-        {/*like this will cause a request to the server, we should modify the normal behaviour of the browser, but there's the Link tag that does this for us*/}
-        404! / <a href="/">Go home page</a>
-        {/*Client routing*/}
-        404! / <Link to="/">Go home page</Link> 
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-const routes = (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <Switch>
-                    <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/*exact true, if not it will match it everytime*/}
-                    <Route path="/create" component={AddExpensePage} />
-                    <Route path="/edit" component={EditExpensePage} />
-                    <Route path="/help" component={HelpPage} />
-                    <Route component={NotFound404Page} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        This is my add expense stateless component
+    </div>
+);
+
+export default AddExpensePage;
