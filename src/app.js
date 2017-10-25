9988c2808e95939224e06fabb2fbe1141a9457fd
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-25 13:50:21 +0200

    Linking between routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import '../node_modules/normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -40,22 +40,26 @@ const NotFound404Page = () => (
 const Header = () => (
     <header>
         <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
     </header>
 );
 
 const routes = (
-    <div>
-        <Header />
         <BrowserRouter>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/*exact true, if not it will match it everytime*/}
-                <Route path="/create" component={AddExpensePage} />
-                <Route path="/edit" component={EditExpensePage} />
-                <Route path="/help" component={HelpPage} />
-                <Route component={NotFound404Page} />
-            </Switch>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={ExpenseDashboardPage} exact={true} /> {/*exact true, if not it will match it everytime*/}
+                    <Route path="/create" component={AddExpensePage} />
+                    <Route path="/edit" component={EditExpensePage} />
+                    <Route path="/help" component={HelpPage} />
+                    <Route component={NotFound404Page} />
+                </Switch>
+            </div>
         </BrowserRouter>
-    </div>
 );
 
 ReactDOM.render(routes, document.getElementById('app'));
