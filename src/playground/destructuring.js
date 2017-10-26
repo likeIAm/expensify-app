8288eeaf69e310ccabe18d6a8fa6c8c8cca2c2a1
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-10-26 08:48:58 +0200

    ES6 Array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,31 +1,42 @@
-const person = {
-    name: 'Luca',
-    //age: 32,
-    location: {
-        city: 'Malta',
-        temp: 24
-    }
-};
-console.log(`My name is ${person.name} and my age is ${person.age} and I live in ${person.location.city}`);
-// Destructuring
-// const { name, age } = person;
-// console.log(`My name is ${name} and my age is ${age}`);
+//
+// Destructuring Array
+//
 
-const { name: firstName, age = 'Unknown is default value' } = person;
-console.log(`My name is ${firstName} and my age is ${age}`);
+const address = ['Via XX Settembre 112', 'Roveredo in Piano', /*'Pordenone'*/, 'Italy'];
+const [, city, province = 'Unknown province', country] = address;
+console.log(`I'm living in ${city}, in province of ${province}`);
 
-const {city, temp} = person.location;
-if (city && temp) {
-    console.log(`And I live in ${city} with a temperature of ${temp}`);
-}
+// //
+// // Destrtucturing Object
+// //
+// const person = {
+//     name: 'Luca',
+//     //age: 32,
+//     location: {
+//         city: 'Malta',
+//         temp: 24
+//     }
+// };
+// console.log(`My name is ${person.name} and my age is ${person.age} and I live in ${person.location.city}`);
+// // Destructuring
+// // const { name, age } = person;
+// // console.log(`My name is ${name} and my age is ${age}`);
 
-const book = {
-    title: 'Che te lo dissi',
-    author: 'Wewe',
-    publisher: {
-        //name: 'Punguin'
-    }
-};
+// const { name: firstName, age = 'Unknown is default value' } = person;
+// console.log(`My name is ${firstName} and my age is ${age}`);
 
-const { name: publisherName = 'Self published'} = book.publisher;
-console.log(`The publisher is ${publisherName}`);
+// const {city, temp} = person.location;
+// if (city && temp) {
+//     console.log(`And I live in ${city} with a temperature of ${temp}`);
+// }
+
+// const book = {
+//     title: 'Che te lo dissi',
+//     author: 'Wewe',
+//     publisher: {
+//         //name: 'Punguin'
+//     }
+// };
+
+// const { name: publisherName = 'Self published'} = book.publisher;
+// console.log(`The publisher is ${publisherName}`);
