50c381bde22a6f2cfa7ddffb712d8f91f3e09b44
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-11-15 15:17:37 +0100

    Replaced test files with the udemy ones

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -9,7 +9,7 @@ const getVisibleExpenses = (expenses, { startDate, endDate, text, sortBy }) => {
         return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
         if (sortBy === 'date') {
-            return a.createdAt < b.createdAt ? -1 : 1; 
+            return a.createdAt < b.createdAt ? 1 : -1; 
         } else if (sortBy === 'amount') {
             return a.amount > b.amount ? -1 : 1;// strange -1 means that a is putted in the array before than b, means with a lower index
         }
