eb351ec4758790e1bc9b0bccf5ff58cf87d31410
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-11-15 16:40:26 +0100

    Enzyme - Installing and configuring

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,13 @@
-import ReactShallowRender from 'react-test-renderer/shallow';
+// import ReactShallowRender from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRender();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-    console.log(renderer.getRenderOutput());
+    // const renderer = new ReactShallowRender();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // console.log(renderer.getRenderOutput());
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h1').text()).toBe('Expensify');
 });
