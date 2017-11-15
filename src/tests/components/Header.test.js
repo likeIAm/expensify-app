c6bdf5134371f54b2677b41ffe5c64ff9c6d33cf
olivaluca85@gmail.com <olivaluca85@gmail.com>
2017-11-15 16:56:29 +0100

    yarn add enzyme-to-json

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,8 @@
-// import ReactShallowRender from 'react-test-renderer/shallow';
-import { shallow } from 'enzyme';
 import React from 'react';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-    // const renderer = new ReactShallowRender();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
-    // console.log(renderer.getRenderOutput());
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify');
+    expect(wrapper).toMatchSnapshot();
 });
